e25cdb7b125e5236aa0c8f89f3af9cf314ca9bf5
MrVlad0567 <mr.vlad0567@gmail.com>
2024-01-13 07:05:56 +0500

    1

---
+++ ToDoApp/src/components/Navigator.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<nav class="navigation">
+    <a class="navigation__item" href="/">Главная</a>
+    <a class="navigation__item" href="/contacts">Контакты</a>
+</nav>
+
+<style>
+    .navigation {
+        display: flex;
+        gap: 30px;
+    }
+    .navigation__item {
+        text-decoration: none;
+        color: green;
+    }
+</style>
---
+++ ToDoApp/src/routes/contacts/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Navigator from "../../components/Navigator.svelte";
+</script>
+
+<Navigator/>
+
+<h1>Contacts</h1>
